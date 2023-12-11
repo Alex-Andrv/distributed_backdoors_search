828b30b5c91bb0348fb2037b1e8e22ccba41eba4
Alex Andreev <sesh_1.0@niuitmo.ru>
2023-12-11 18:43:46 +0300

    ready

--- run_MultColumn32x32_randinp_1.cnf.sh
+++ run_MultColumn32x32_randinp_1.cnf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 source activate multithreading_solver
 cd ~/distributed_backdoors_search
-redis-server --port 6381 &
+redis-server redis.conf --port 6381 &
 PID=$!
 python start_solve.py ./data/mults_inversion_cnfs/MultColumn32x32_randinp_1.cnf --max-learning 15 --redis-port 6381 --max-buffer-size 10000 -tmp ./backdoor-producer/tmp/MultColumn32x32_randinp_1 --log-dir ./log/MultColumn32x32_randinp_1 -n 5 -er 1 -er 1 -er 1 -er 1 -er 1 -es 10 -es 10 -es 12 -es 12 -es 14 -ei 1000 -ei 1200 -ei 1500 -ei 1500 -ei 2000 -c 0 -c 0 -c 1000 -c 2000 -c 3000
 redis-cli SHUTDOWN
